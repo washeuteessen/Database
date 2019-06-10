944e75ae8462f4d7c1c5e71c7789535fc13c94fe
Valentin <valentin.gabriel.kuhn@outlook.de>
2019-06-10 21:35:27 +0200

    erster Test

---
+++ startMongoDB_instance.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\MongoDB\Server\4.0\bin\mongo.exe" 
+mongo
+pause >nul
---
+++ startMongoDB_server.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath z:\data\db --bind_ip 127.0.0.1
+pause
